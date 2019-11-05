e8a302c98f71d4cc440c2071dcc227f8e38fc2f4
Akashdeep-S9 <akasdeeps19@gmail.com>
2019-11-05 20:57:57 +0530

    initial commit

--- col_detection.py
+++ col_detection.py
@@ -1,18 +1,29 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture(0)
+# quality = 400
+# im = cv2.imread("F1.large.jpg")
+video_path = input("Enter video path or 'live' for live capture : ")
+quality = input("Enter quality (300,400,500) : ")
+quality = int(quality)
+if video_path == "live":
+  cap = cv2.VideoCapture(0)
+else:
+  cap = cv2.VideoCapture(video_path)
 
 def preprocessing(img_org):
-  img = cv2.resize(img_org,(300,300),cv2.INTER_AREA)
+  img = cv2.resize(img_org,(quality,quality),cv2.INTER_AREA)
   hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
   lower_blue = np.array([0,50,50])
   upper_blue = np.array([10,255,255])
-  mask1 = cv2.inRange(hsv, np.array([0, 120, 110]), np.array([8, 255, 255]))
-  mask2 =  cv2.inRange(hsv,np.array([172, 120, 110]), np.array([180, 255, 255]))
+  mask1 = cv2.inRange(hsv, np.array([0, 115, 105]), np.array([10, 255, 255]))
+  mask2 =  cv2.inRange(hsv,np.array([170, 115, 105]), np.array([180, 255, 255]))
   mask = mask1+mask2  
   res = cv2.bitwise_and(img,img, mask= mask)
-  im_gray = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
+  # cv2.imshow(None,res)
+  # cv2.waitKey()
+  im_gray = cv2.cvtColor(res, cv2.COLOR_HSV2BGR)
+  im_gray = cv2.cvtColor(im_gray,cv2.COLOR_BGR2GRAY)
   (thresh, im_bw) = cv2.threshold(im_gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
   im_bw = cv2.medianBlur(im_bw,3)
   
@@ -20,8 +31,8 @@ def preprocessing(img_org):
   regions_x = [[] for i in range(ret)]
   regions_y = [[] for i in range(ret)]
   img_res = img
-  for i in range(300):
-    for j in range(300):
+  for i in range(quality):
+    for j in range(quality):
       if(labels[i,j] > 0):
           regions_x[labels[i,j]].append(i)
           regions_y[labels[i,j]].append(j)
@@ -31,6 +42,7 @@ def preprocessing(img_org):
     img_res = cv2.rectangle(img,s_point,e_point,(255,0,0),1)
   return img_res
 
+#preprocessing(im)
 if (cap.isOpened()== False): 
   print("Error opening video stream or file")
  
@@ -47,3 +59,4 @@ while(cap.isOpened()):
 
 cap.release()
 cv2.destroyAllWindows()
+
